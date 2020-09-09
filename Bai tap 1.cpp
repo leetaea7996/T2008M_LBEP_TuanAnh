3a01bf434c5d76d198b22d50d06bb5ab9a4f0ff4
leetaea7996 <70894211+leetaea7996@users.noreply.github.com>
2020-09-09 09:19:19 +0700

    Create Bai tap 1.cpp

---
+++ Bai tap 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int num;
+	printf("Nhap num=");
+	scanf("%d",&num);
+	int r;
+	r= num%2;
+	if(r==0){
+		// Dung
+		printf("%d so chan",num);
+	}else{
+		// Sai
+		printf("%d so le",num);
+	}
+		
+}
